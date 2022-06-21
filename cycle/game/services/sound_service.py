d3b438ab4615acb5749f640507b13414412fa443
ryanscom <ryanscom@byui.edu>
2022-06-21 09:53:05 -0700

    music service test

---
+++ cycle/game/services/sound_service.py
@@ -0,0 +1,35 @@
+import pygame
+import os
+
+def play_music(midi_filename):
+  '''Stream music_file in a blocking manner'''
+  clock = pygame.time.Clock()
+  pygame.mixer.music.load(midi_filename)
+  pygame.mixer.music.play()
+  while pygame.mixer.music.get_busy():
+    clock.tick(30) # check if playback has finished
+    
+midi_filename = os.path.abspath('cse210-09\cycle\game\music\daft_punk.mid')
+
+# mixer config
+freq = 44100  # audio CD quality
+bitsize = -16   # unsigned 16 bit
+channels = 2  # 1 is mono, 2 is stereo
+buffer = 1024   # number of samples
+pygame.mixer.init(freq, bitsize, channels, buffer)
+
+# optional volume 0 to 1.0
+pygame.mixer.music.set_volume(0.8)
+
+# listen for interruptions
+try:
+  # use the midi file you just saved
+  play_music(midi_filename)
+except KeyboardInterrupt:
+  # if user hits Ctrl/C then exit
+  # (works only in console mode)
+  pygame.mixer.music.fadeout(1000)
+  pygame.mixer.music.stop()
+  raise SystemExit
+
+play_music(os.path.abspath('cse210-09\cycle\game\music\daft_punk.mid'))
